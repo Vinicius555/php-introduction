231a65b7089c522d6fa17a74fb104e9f7c54e7b5
Vinicius <vinicius-gabriel-guilherme@hotmail.com>
2022-11-30 17:24:12 -0300

    create teste

--- app_gestao/app/Http/Controllers/TesteController.php
+++ app_gestao/app/Http/Controllers/TesteController.php
@@ -8,6 +8,8 @@ class TesteController extends Controller
 {
     public function teste(int $p1, int $p2)
     {
-        return view('site.teste', ['x' => $p1, 'y' => $p2]);
+       // return view('site.teste', ['p1' => $p1, 'p2' => $p2]); --ArrayAssociativo
+       return view('site.teste',compact('p1','p2')); // metodo compact passase a nome da variavel como um string
+       //return view('site.teste')->with('p1',$p1)->with('p2',$p2); // metodo with passa primeiro o nome que sera assciado a view e depois a variavel correspondente
     }
 }
