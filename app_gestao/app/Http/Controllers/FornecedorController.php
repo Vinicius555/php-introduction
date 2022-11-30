0bfa79aa99bd7ce2b8869c0e2a5a42b61e8111a9
Vinicius <vinicius-gabriel-guilherme@hotmail.com>
2022-11-30 17:26:18 -0300

    create fornecedores

---
+++ app_gestao/app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FornecedorController extends Controller
+{
+    public function index(){
+        $fornecedor = [
+            0 => ['nome'=>'fornecedor 1',
+            'status'=>'N',
+            'cnpj'=> '0000.000/000',
+            'DDD' => '11',
+            'telefone'=>'0000-0000'],
+            1 => ['nome'=>'fornecedor 2',
+            'status'=>'S',
+            'cnpj' => null,
+            'DDD' => '85',
+            'telefone'=>'0000-0000'],
+            2 => ['nome'=>'fornecedor 3',
+            'status'=>'S',
+            'cnpj' => null,
+            'DDD' => '87',
+            'telefone'=>'0000-0000'],
+            3 => ['nome'=>'fornecedor 4',
+            'status'=>'S',
+            'cnpj' => '000.000/000',
+            'DDD' => '89',
+            'telefone'=>'0000-0000'],
+        ];
+        $fornecedors = [];
+        return view('app.fornecedores.index',compact('fornecedors'));
+
+        
+        
+    }
+}
