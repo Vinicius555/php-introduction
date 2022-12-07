caea5a2546a113a56445e5523e11a271990105c7
Vinicius Gabriel <vinicius-gabriel-guilherme@hotmail.com>
2022-12-06 21:50:38 -0300

    modified contato

--- app_gestao/resources/views/site/contato.blade.php
+++ app_gestao/resources/views/site/contato.blade.php
@@ -3,21 +3,6 @@
 @section('titulo', $titulo)
     
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="img/logo.png">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
             <h1>Entre em contato conosco</h1>
@@ -25,24 +10,10 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form>
-                    <input type="text" placeholder="Nome" class="borda-preta">
-                    <br>
-                    <input type="text" placeholder="Telefone" class="borda-preta">
-                    <br>
-                    <input type="text" placeholder="E-mail" class="borda-preta">
-                    <br>
-                    <select class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="">Dúvida</option>
-                        <option value="">Elogio</option>
-                        <option value="">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                @component('site.layouts._components.form_contato')
+                    <p>A nossa equipe analisará a sua mensagem e retornaremos o mais brevemente possivel.</p>
+                    <p>Nosso tempo médio de respota é de 48 horas.</p>
+                @endcomponent
             </div>
         </div>
     </div>
@@ -50,9 +21,9 @@
     <div class="rodape">
         <div class="redes-sociais">
             <h2>Redes sociais</h2>
-            <img src="img/facebook.png">
-            <img src="img/linkedin.png">
-            <img src="img/youtube.png">
+            <img src="{{asset('img/facebook.png')}}">
+            <img src="{{asset('img/linkedin.png')}}">
+            <img src="{{asset('img/youtube.png')}}">
         </div>
         <div class="area-contato">
             <h2>Contato</h2>
@@ -62,7 +33,7 @@
         </div>
         <div class="localizacao">
             <h2>Localização</h2>
-            <img src="img/mapa.png">
+            <img src="{{asset('img/mapa.png')}}">
         </div>
     </div>
 
